6012eb6cc25ab628133c8b721e0926861ff90695
Jhonatan Medeiros <55262464+iamjhonatan@users.noreply.github.com>
2020-12-31 01:54:33 -0300

    Implementação do projeto concluída.

---
+++ src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -0,0 +1,111 @@
+package br.com.cod3r.cm.modelo;
+
+import br.com.cod3r.cm.excecao.ExplosaoException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Tabuleiro {
+
+    private int linhas;
+    private int colunas;
+    private int minas;
+
+    private final List<Campo> campos = new ArrayList<>();
+
+    public Tabuleiro(int linhas, int colunas, int minas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+
+        gerarCampos();
+        associarVizinhos();
+        sortearMinas();
+    }
+
+    public void abrir(int linha, int coluna){
+        try {
+            campos.parallelStream()
+                    .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                    .findFirst() // Optional<Campo>
+                    .ifPresent(c -> c.abrir());
+        } catch (ExplosaoException e){
+            campos.forEach(c -> c.setAberto(true));
+            throw e;
+        }
+    }
+
+    public void alterarMarcacao(int linha, int coluna){
+        campos.parallelStream()
+                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                .findFirst() // Optional<Campo>
+                .ifPresent(c -> c.alternarMarcacao());
+    }
+
+    private void gerarCampos() {
+        for (int linha = 0; linha < linhas; linha++){
+            for(int coluna = 0; coluna < colunas; coluna++){
+                campos.add(new Campo(linha, coluna));
+            }
+        }
+    }
+
+    private void associarVizinhos() {
+        for (Campo c1: campos){
+            for (Campo c2: campos){
+                c1.adicionarVizinho(c2);
+            }
+        }
+    }
+
+    // Quando as minhas armadas forem igual o número de minas determinado, o laço Do/While é encerrado
+    private void sortearMinas() {
+        long minasArmadas = 0;
+        Predicate<Campo> minado =  c -> c.isMinado();
+
+        do {
+            int aleatorio = (int) (Math.random() * campos.size()); // Gerando um valor aleatório entre 0 e o tamamho da lista -1, fazendo o CAST para 'int'.
+            campos.get(aleatorio).minar();
+            minasArmadas = campos.stream().filter(minado).count();
+        } while(minasArmadas < minas);
+    }
+
+    // Se todos os campos tem o objetivo alcançado (deram 'match'), o objetivo foi alcançado
+    public boolean objetivoAlcancado(){
+        return campos.stream().allMatch(c -> c.objetivoAlcancado());
+    }
+
+    public void reiniciar(){
+        campos.stream().forEach(c -> c.reiniciar());
+        sortearMinas();
+    }
+
+    // Sempre que houver mutas concatenações a fazer, o StringBuilder é uma boa opção, até pensando em processamento de memória
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("  ");
+        for (int c = 0; c < colunas; c++){
+            sb.append(" ");
+            sb.append(c);
+            sb.append(" ");
+        }
+
+        sb.append("\n");
+
+        int i = 0;
+        for(int l = 0; l < linhas; l++){
+            sb.append(l);
+            sb.append(" ");
+            for (int c = 0; c < colunas; c++){
+                sb.append(" ");
+                sb.append(campos.get(i));
+                sb.append(" ");
+                i++;
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
